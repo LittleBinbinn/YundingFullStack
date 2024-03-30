ec80bc3d7b4914a3299cd7b3e75791a6f3b49744
JYQ0928005 <152002380+JYQ0928005@users.noreply.github.com>
2024-03-30 21:59:23 +0800

    全栈-江雨晴-Vue第二次任务

---
+++ src/八期学员文件夹/江雨晴/全栈-江雨晴-Vue第二次任务/components/TableHead.vue
@@ -0,0 +1,14 @@
+<template>
+    <tr style="height: 40px;" border="2">
+        <td class="td1" style="width: 70px;">序号</td>
+        <td class="td2" style="width: 300px;">书名</td>
+        <td class="td3" style="width: 90px;">价格</td>
+        <td class="td4" style="width: 100px;">购买数量</td>
+        <td class="td5" style="width: 90px;">操作</td>
+    </tr>
+</template>
+<script>
+export default {
+    name:'TableHead',
+}
+</script>
