9b3a9431fdfe3bd7df86b2511923851b7c463578
yangjinni <3400804545@qq.com>
2024-04-01 19:52:44 +0800

    全栈-杨金霓-Vue第二次任务

---
+++ src/八期学员文件夹/杨金霓/全栈-杨金霓-Vue第二次任务/src/components/totalPrice.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="total">
+        <h2>总价:￥{{all}}</h2>
+        <h3>当前选中的书籍：{{bookk}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
