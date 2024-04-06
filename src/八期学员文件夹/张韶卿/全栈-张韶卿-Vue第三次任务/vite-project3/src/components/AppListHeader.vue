3b3f2dada8779891bc58e4b6fd36a6c629f9d01f
zhangshaoqing222 <147579088+zhangshaoqing222@users.noreply.github.com>
2024-04-06 21:25:02 +0800

    Add files via upload

---
+++ src/八期学员文件夹/张韶卿/全栈-张韶卿-Vue第三次任务/vite-project3/src/components/AppListHeader.vue
@@ -0,0 +1,54 @@
+<template>
+    <div :class="bangClass">{{ list.name }}</div>
+    <img :src="list.coverImgUrl" :class="picClass" />
+    <div :class="jieClass">{{ list.description }}</div>
+
+</template>
+
+
+
+
+<script>
+export default {
+    name: 'AppListHeader',
+    props: {
+        list: {
+            type: Object,
+            required: true
+        },
+        
+    },
+    
+    data() {
+        return {
+            picClass: 'pic',
+            bangClass: 'bang',
+            jieClass:'jie'
+        }
+    }
+    }
+</script>
+
+
+
+<style scoped>
+.pic{
+    position: relative;
+    left: 170px;
+    top: -10px;
+    width: 100px;
+    height: auto;
+}
+.bang{
+    position: relative;
+    top: 48px;
+    left: 39px;
+    font-weight: bold;
+    font-size: large
+    /* background-color: #c43535; */
+}
+.jie{
+    width: 500px;
+}
+
+</style>
