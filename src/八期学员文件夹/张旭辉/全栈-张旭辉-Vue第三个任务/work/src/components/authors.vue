2153e17b785ea2f5ae9894032e29cb6b6cdd8534
Zhang XUhui <2413014295@qq.com>
2024-04-06 22:41:12 +0800

    全栈-张旭辉-Vue第三次任务

---
+++ src/八期学员文件夹/张旭辉/全栈-张旭辉-Vue第三个任务/work/src/components/authors.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>热门歌手</h2>
+        <h2><hr></h2>
+        <slot name="artist"></slot>
+        <h2><hr></h2>
+    </div>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
+
+<style scoped>
+  hr{
+    background-color: black;
+    height:2px;
+    width:100%;
+  }  
+</style>
