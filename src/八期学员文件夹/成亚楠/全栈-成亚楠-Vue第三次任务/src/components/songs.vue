824ecb4b57da0fba462dc89860b7319604e5616f
chengyanan <3114395554@qq.com>
2024-04-08 01:02:05 +0800

    全栈-成亚楠-Vue第三次任务

---
+++ src/八期学员文件夹/成亚楠/全栈-成亚楠-Vue第三次任务/src/components/songs.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="back">
+        <div class="left">
+            <h2>{{ title }}</h2>
+            <p>{{ songText }}</p>
+
+        </div>
+        <img :src=" songImg " alt="">
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: ["title","songText","songImg"]
+}
+</script>
+
+<style>
+.back{
+    display: flex;
+    margin-bottom: 30px;
+    margin-left: 10px;
+}
+.left{
+    display: flex;
+    width: 280px;
+    flex-direction: column;
+}
+img{
+    width: 140px;
+    height: 140px;
+    margin-left: 100px;
+}
+p{
+    margin-top: -20px;
+}
+h2{
+    font-size: 35px;
+    margin-top: 0px;
+}
+</style>
