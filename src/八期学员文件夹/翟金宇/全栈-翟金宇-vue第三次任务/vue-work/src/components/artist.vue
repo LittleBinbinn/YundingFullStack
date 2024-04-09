a240f60a34a59e7dca67bfc8ebba52593b42bdb1
翟金宇 <3422801092@qq.com>
2024-04-09 14:48:10 +0800

    全栈-翟金宇-vue第三次任务

---
+++ src/八期学员文件夹/翟金宇/全栈-翟金宇-vue第三次任务/vue-work/src/components/artist.vue
@@ -0,0 +1,24 @@
+<template>
+  <span class="singer">
+    <div>
+      <slot name="head"></slot>
+    </div>
+    <div>
+      <slot name="name"></slot>
+    </div>
+  </span>
+</template>
+
+<script>
+export default{
+  
+}
+</script>
+
+<style scoped>
+.singer{
+  display: inline-block;
+  position: relative;
+  width: 100px;
+}
+</style>
