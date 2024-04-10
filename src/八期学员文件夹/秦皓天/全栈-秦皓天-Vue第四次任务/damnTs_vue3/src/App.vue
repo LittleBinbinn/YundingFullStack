ad9ac9d9f1afc2f61a801efbe63308dcb4827602
Qinht <1586791319@qq.com>
2024-04-10 13:11:04 +0800

    全栈-秦皓天-Vue第四次任务

---
+++ src/八期学员文件夹/秦皓天/全栈-秦皓天-Vue第四次任务/damnTs_vue3/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="music">
+        <showInfo/>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import showInfo from "./components/showInfo.vue"
+</script>
+
+<style scoped>
+</style>
