a0a5cccf5e933e7f72ba46093f7c6da5cbc8d6cd
lucan <1765907293@qq.com>
2024-04-11 08:49:38 +0800

    全栈-卢灿-Vue第三次任务

---
+++ src/八期学员文件夹/卢灿/全栈-卢灿-Vue第三次任务/demo/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="app">
+    <h4>热门歌手</h4>
+    <hr>
+    <authors :giveAugiveArtsits="artists"></authors>
+    <hr>
+    <h4>榜单</h4>
+    <hr>
+    <div class="billboard">
+      <mode style="margin-right: 75px;" :imgUrl="newSong.coverImgUrl">
+      <template v-slot:title>{{ newSong.name }}</template>
+      <template v-slot:world>{{ newSong.description }}</template>
+      <template v-slot:songName>
+          <li v-for="(item,index) in newSong.tracks" :key="index">
+          {{ item.name }}
+          </li>
+      </template>
+      </mode>
+      <mode style="margin-right: 75px;" :imgUrl="hotSong.coverImgUrl">
+      <template v-slot:title>{{ hotSong.name }}</template>
+      <template v-slot:world>{{ hotSong.description }}</template>
+      <template v-slot:songName>
+          <li v-for="(item,index) in hotSong.tracks" :key="index">
+          {{ item.name }}
+          </li>
+      </template>
+      </mode>
+      <mode :imgUrl="originSong.coverImgUrl">
+      <template v-slot:title>{{ originSong.name }}</template>
+      <template v-slot:world>{{ originSong.description }}</template>
+      <template v-slot:songName>
+          <li v-for="(item,index) in originSong.tracks" :key="index">
+          {{ item.name }}
+          </li>
+      </template>
+      </mode>
+    </div>
+    <hr>
+  </div>
+</template>
+
+<script>
+import authors from './components/authors.vue'
+import mode from './components/mode.vue'
+import artists from '@/data/artist'
+import hotSong from '@/data/hotSong'
+import newSong from '@/data/newSong'
+import originSong from '@/data/originSong'
+export default {
+  components: { mode , authors},
+  data() {
+    return{
+      artists: artists,
+      hotSong: hotSong,
+      newSong: newSong,
+      originSong: originSong,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.billboard{
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+}
+li{
+  margin-bottom: 5px;
+}
+</style>
