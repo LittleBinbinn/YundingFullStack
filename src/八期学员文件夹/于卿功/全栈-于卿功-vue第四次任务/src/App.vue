ed29597e709f91e8b27e84e93014f68c64c295bb
yuqinggong <2050626871@qq.com>
2024-04-20 00:33:55 +0800

    全栈-于卿功-vue第四次任务

---
+++ src/八期学员文件夹/于卿功/全栈-于卿功-vue第四次任务/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import list from "./components/list.vue";
+</script>
+
+<template>
+  <list></list>
+  <song></song>
+</template>
+
+
+<style scoped>
+
+
+</style>
---
+++ src/八期学员文件夹/于卿功/全栈-于卿功-vue第四次任务/src/components/song.vue
@@ -0,0 +1,16 @@
+<script setup>
+import songs from "./song2.vue";
+import hotSong from "../hotSong.js";
+import newSong from "../newSong.js";
+import originSong from "../originSong.js";
+</script>
+<template>
+  <div>
+    <h2>榜单</h2>
+    <hr/>
+      <songs></songs>
+  </div>
+</template>
+
+<style scoped>
+</style>
