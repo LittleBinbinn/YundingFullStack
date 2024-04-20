b59dd1da14043ea1682178b13ebeff555d8c5d1e
sandwichzhang <3234643550@qq.com>
2024-04-20 21:04:50 +0800

    全栈-张世杰-Vue第四次任务

---
+++ src/八期学员文件夹/张世杰/Vue任务/全栈-张世杰-Vue第四次任务/src/components/MusicListItem.vue
@@ -0,0 +1,37 @@
+<template>  
+  <div class="music-list-item">  
+    <img :src="item.image" :alt="item.name" class="musician-image" />  
+    <p class="musician-name">{{ item.name }}</p>  
+  </div>  
+</template>  
+  
+<script setup>  
+import { defineProps } from 'vue';  
+  
+defineProps({  
+  item: {  
+    type: Object,  
+    required: true,  
+  },  
+});  
+  
+// 如果还有其他响应式状态或方法，可以在这里定义  
+// 但在这个组件中，我们仅依赖传入的 props  
+</script>  
+  
+<style scoped>  
+.music-list-item {  
+  display: flex;  
+  flex-direction: column;  
+  align-items: center;  
+  margin: 20px;  
+  margin-bottom: 10px;  
+}  
+  
+.musician-image {  
+  width: 100px;  
+  height: 100px;  
+  object-fit: cover;  
+  margin-bottom: 5px;  
+}  
+</style>
