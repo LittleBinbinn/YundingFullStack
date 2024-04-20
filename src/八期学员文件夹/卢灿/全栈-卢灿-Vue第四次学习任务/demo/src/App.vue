09b45add671472340f637da596ef4b0901ff6f8c
lucan <1765907293@qq.com>
2024-04-20 20:00:49 +0800

    全栈-卢灿-Vue第四次任务

---
+++ src/八期学员文件夹/卢灿/全栈-卢灿-Vue第四次学习任务/demo/src/App.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { ref } from 'vue';
+import authors from './components/authors.vue';
+import mode from './components/mode.vue';
+import artist from '../data/artist';
+import hotSongs from '../data/hotSong'
+import newSongs from '../data/newSong'
+import originSongs from '../data/originSong'
+const artists = ref(artist);
+const hotSong = ref(hotSongs);
+const newSong = ref(newSongs);
+const originSong = ref(originSongs);
+</script>
+
+<template>
+  <div class="app">
+    <h4>热门歌手</h4>
+    <hr>
+    <authors :giveAugiveArtsits="artists"></authors>
+    <hr>
+    <h4>榜单</h4>
+    <hr>
+    <div class="billboard">
+      <mode style="margin-right: 75px;" :imgUrl="newSong.coverImgUrl">
+        <template v-slot:title>{{ newSong.name }}</template>
+        <template v-slot:world>{{ newSong.description }}</template>
+        <template v-slot:songName>
+          <li v-for="(item, index) in newSong.tracks" :key="index">
+            {{ item.name }}
+          </li>
+        </template>
+      </mode>
+      <mode style="margin-right: 75px;" :imgUrl="hotSong.coverImgUrl">
+        <template v-slot:title>{{ hotSong.name }}</template>
+        <template v-slot:world>{{ hotSong.description }}</template>
+        <template v-slot:songName>
+          <li v-for="(item, index) in hotSong.tracks" :key="index">
+            {{ item.name }}
+          </li>
+        </template>
+      </mode>
+      <mode :imgUrl="originSong.coverImgUrl">
+        <template v-slot:title>{{ originSong.name }}</template>
+        <template v-slot:world>{{ originSong.description }}</template>
+        <template v-slot:songName>
+          <li v-for="(item, index) in originSong.tracks" :key="index">
+            {{ item.name }}
+          </li>
+        </template>
+      </mode>
+    </div>
+    <hr>
+  </div>
+</template>
+
+<style scoped>
+.billboard {
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+}
+
+li {
+  margin-bottom: 5px;
+}
+</style>
---
+++ src/八期学员文件夹/卢灿/全栈-卢灿-Vue第四次学习任务/demo/src/components/authors.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps({
+  giveAugiveArtsits: {
+    type: Object,
+    default: {}
+  }
+})
+</script>
+
+<template>
+  <div class="author">
+    <div v-for="(item, index) in giveAugiveArtsits" :key="index" class="box">
+      <img :src="item.picUrl">
+      <span>{{ item.name }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 100px;
+  height: auto;
+}
+
+.author {
+  display: flex;
+  justify-content: flex-start;
+}
+
+.box {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 100%;
+}
+
+span {
+  width: 100px;
+  text-align: center;
+}
+</style>
