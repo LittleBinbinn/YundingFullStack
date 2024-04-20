d4a4083e73cf9f1d2b9b7a44fc09ee63cab9779a
zhangshaoqing222 <147579088+zhangshaoqing222@users.noreply.github.com>
2024-04-20 19:15:59 +0800

    Add files via upload

---
+++ src/八期学员文件夹/张韶卿/全栈-张韶卿-Vue第四次任务/vite-project4/src/components/AppFrame.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <slot name="title"></slot>
+        <hr/>
+        <slot name="content"></slot>
+        <hr/>
+    </div>
+
+</template>
+
+
+
+
+<script setup>
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/八期学员文件夹/张韶卿/全栈-张韶卿-Vue第四次任务/vite-project4/src/components/AppListHeader.vue
@@ -0,0 +1,50 @@
+<template>
+    <div :class="bangClass">{{ list.name }}</div>
+    <img :src="list.coverImgUrl" :class="picClass" />
+    <div :class="jieClass">{{ list.description }}</div>
+
+</template>
+
+
+<script setup>
+import { ref, defineProps, onMounted } from 'vue';
+const props = defineProps({
+    list: {
+        type: Object,
+        required: true
+    }
+});
+
+const picClass = ref('pic');
+const bangClass = ref('bang');
+const jieClass = ref('jie');
+
+onMounted(() => {
+    console.log(props.list); // 检查 list prop 是否正确传递  
+});
+
+</script>
+
+
+
+<style scoped>
+.pic{
+    position: relative;
+    left: 170px;
+    top: -10px;
+    width: 100px;
+    height: auto;
+}
+.bang{
+    position: relative;
+    top: 48px;
+    left: 39px;
+    font-weight: bold;
+    font-size: large;
+    /* background-color: #c43535; */
+}
+.jie{
+    width: 500px;
+}
+
+</style>
