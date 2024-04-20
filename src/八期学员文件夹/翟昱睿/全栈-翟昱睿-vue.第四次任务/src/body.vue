b7b322d57c260d456b2975abd22ed6ea701e62fd
ys828 <1689398113@qq.com>
2024-04-20 19:17:23 +0800

    全栈-翟昱睿-vue第四次任务

---
+++ src/八期学员文件夹/翟昱睿/全栈-翟昱睿-vue.第四次任务/src/body.vue
@@ -0,0 +1,101 @@
+    <template>
+        <div class="body">
+            <h2 class="text1">榜单</h2>
+            <hr style="border: 2px solid black;">
+            
+        </div>
+        <div class="bady">
+            <bodypart>
+                <template v-slot:textone>
+                    <h2>{{ newSong.name }}</h2>
+                </template>
+                <template v-slot:texttwo>
+                    <h3>{{ newSong.description }}</h3>
+                </template>
+                <template v-slot:img>
+                    <img :src="newSong.coverImgUrl" class="imging" />
+                </template>
+                <template v-slot:song>
+                    <div v-for="(song, index ) in newSong.tracks" :key="index" class="texting">
+                        {{ index + 1 }}-{{ song.name }}
+                    </div>
+                </template>
+            </bodypart>
+
+
+            <bodypart>
+                <template v-slot:textone>
+                    <h2>{{ hotSong.name }}</h2>
+                </template>
+                <template v-slot:texttwo>
+                    <h3>{{ hotSong.description }}</h3>
+                </template>
+                <template v-slot:img>
+                    <img :src="hotSong.coverImgUrl" class="imging" />
+                </template>
+                <template v-slot:song>
+                    <div v-for="(song, index ) in hotSong.tracks" :key="index" class="texting">
+                        {{ index + 1 }}-{{ song.name }}
+                    </div>
+                </template>
+            </bodypart>
+
+
+            <bodypart>
+                <template v-slot:textone>
+                    <h2>{{ originSong.name }}</h2>
+                </template>
+                <template v-slot:texttwo>
+                    <h3>{{ originSong.description }}</h3>
+                </template>
+                <template v-slot:img>
+                    <img :src="originSong.coverImgUrl" class="imging" />
+                </template>
+                <template v-slot:song >
+                    <div v-for="(song, index ) in originSong.tracks" :key="index" class="texting">
+                        {{ index + 1 }}-{{ song.name }}
+                    </div>
+                </template>
+            </bodypart>
+        </div>
+    </template>
+    <script>
+        import bodypart from './bodypart'
+        import originSong from './originSong.js';
+        import newSong from './newSong.js';
+        import hotSong from './hotSong.js';
+        import {ref} from 'vue'
+        export default {
+            components:{
+                bodypart
+            },
+            setup(){
+                return{
+                    originSong:ref(originSong),
+                    newSong:ref(newSong),
+                    hotSong:ref(hotSong)
+                }
+            }
+                
+        }
+    </script>
+    <style scoped>
+        .body{
+            width: 100%;
+        }
+        .text1{
+            font-size: 25px;
+        }
+        .bady{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+        }
+        .imging{
+            width: 200px;
+        }
+        .texting{
+            padding: 10px;
+            font-size: 15px;
+        }
+    </style>
---
+++ src/八期学员文件夹/翟昱睿/全栈-翟昱睿-vue.第四次任务/src/head.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="head">
+        <h2 class="text1">热门歌手</h2>
+        <hr style="border: 2px solid black;"><br>
+        <div class="headpics">
+            <slot v-for="(pic,index) in pics" :key="index">
+                <div class="headslot">
+                    <img :src="pic.picUrl" :alt="pic.altUrl" class="img1" />
+                    <p>{{ pic.name }}</p>
+                </div>
+            </slot>
+            <!-- <headpart>
+                <slot>
+                    
+                </slot>
+            </headpart> -->
+        </div>
+        <hr style="border: 1px solid black;">
+    </div>
+    </template>
+
+<script>
+import {ref} from 'vue'
+import {pics} from './artist.js'
+export default {
+    setup(){
+        const picsref=ref(pics);
+        // onMounted()=>{
+
+        // }
+        return{
+            pics:picsref.value
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+    .text1{
+        font-size: 25px;
+    }
+    .head{
+        width: 100%;
+        height: 240px;
+        /* background-color: orange; */
+    }
+    .headpics{
+        width: 100%;
+        height: 150px;
+        /* background-color: green; */
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    }
+    .headslot{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        height: 150px;
+        align-items: center;
+    }
+    .img1{
+        width: 140px;
+        /* height: 120px; */
+    }
+</style>    
