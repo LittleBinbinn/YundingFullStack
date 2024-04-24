b0c406c71db559bdc60331f4fd091803b1ea2f55
yangzhang666666 <zyyyy_0709@qq.com>
2024-04-24 22:19:47 +0800

    全栈-张扬-Vue第五次任务

---
+++ src/八期学员文件夹/张扬/全栈-张扬-Vue第五次任务/src/views/Module.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Module</h2>
+    <span>{{ module }}</span>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+const route = useRoute()
+let module = route.query.module
+</script>
+
+<style>
+
+</style>
