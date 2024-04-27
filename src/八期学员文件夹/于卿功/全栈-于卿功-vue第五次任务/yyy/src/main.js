e62213d3f31d78fb0fcdd0afc6f22f972ff12dcf
yuqinggong <2050626871@qq.com>
2024-04-27 15:21:51 +0800

    全栈-于卿功-vue第五次任务

--- src/八期学员文件夹/于卿功/全栈-于卿功-vue第五次任务/yyy/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from "./router/router.js";
-window.__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = true;
-const app=createApp(App);
-app.use(router);
-app.mount('#app');
