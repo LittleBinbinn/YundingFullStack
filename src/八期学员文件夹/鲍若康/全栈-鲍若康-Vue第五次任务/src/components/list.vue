c2226c15c23f5814cca144a212d722d8998093ae
baoruokang <bao13934888678@qq.com>
2024-04-29 22:16:02 +0800

    全栈-鲍若康-Vue第五次任务

---
+++ src/八期学员文件夹/鲍若康/全栈-鲍若康-Vue第五次任务/src/components/list.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import { reactive,ref } from 'vue';
+
+const list = reactive([
+    "吃饭", "睡觉", "打豆豆"
+])
+var newlist ="";
+
+const router = useRouter()
+
+function addlist() {
+    list.push(newlist)
+    newlist=""
+    
+}
+function select(index) {
+    router.push({
+        path: "/home/selected",
+        query: {
+            select: list[index]
+        } 
+    })
+
+}
+</script>
+
+<template>
+    <div>
+        <ul>
+            <li v-for="(item , index) in list" @click="select(index)" :key="item">{{ item }}</li>
+        </ul>
+        <input type="text" placeholder="请输入事项" v-model="newlist" @keyup.enter="addlist()" />
+
+
+    </div>
+
+</template>
