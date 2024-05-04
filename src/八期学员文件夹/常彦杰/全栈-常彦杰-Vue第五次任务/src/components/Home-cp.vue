26de97572a8ed98f7981486f8630c84965cf3d8e
Jsama <x18834506311@163.com>
2024-05-04 11:55:54 +0800

    全栈-常彦杰-Vue第五次任务

---
+++ src/八期学员文件夹/常彦杰/全栈-常彦杰-Vue第五次任务/src/components/Home-cp.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="name">我是home组件</div>
+    <div >
+        <router-link to="/home/list" class="gongneng">展开列表</router-link>
+        <router-link to="/home/choice" class="gongneng">你的选择</router-link>
+    </div>
+    
+    <router-view v-slot="{ Component }">
+  <keep-alive>
+    <component :is="Component" />
+  </keep-alive>
+</router-view>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .gongneng {
+        color:rgb(106, 192, 106);  
+        margin-left: 20px;
+    }
+    .name {
+        font-size: 25px;
+        font-weight: 900;
+    }
+</style>
---
+++ src/八期学员文件夹/常彦杰/全栈-常彦杰-Vue第五次任务/src/components/Home-list.vue
@@ -0,0 +1,27 @@
+<template>
+  <input type="text" v-model="choice" @keyup.enter="add">
+    <ul>
+      <li v-for="(item,index) in list" :key="index" @click="mychoice(item)">{{ item }}</li>
+    </ul>
+</template>
+<script setup>
+    import { ref } from 'vue';
+    import { useRouter } from 'vue-router';
+    const router = useRouter();
+    let list = ref(['吃饭','睡觉','发癫']);
+    const choice = ref('')
+const mychoice = function (item) {
+  router.push({
+    path:
+      '/home/choice',
+    query: {
+      info:item
+    }    
+      })
+}
+const add = function () {
+  list.value.push(choice.value);
+}
+</script>
+<style scoped>
+</style>
