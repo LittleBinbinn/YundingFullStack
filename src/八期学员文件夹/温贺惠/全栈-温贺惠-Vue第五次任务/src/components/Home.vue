7eec4d29ed4aa92503231394589427ac4bf4653b
evawhh <153496336+evawhh@users.noreply.github.com>
2024-05-04 15:15:22 +0800

    全栈-温贺惠-Vue第五次任务

---
+++ src/八期学员文件夹/温贺惠/全栈-温贺惠-Vue第五次任务/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>我是home组件</h2>
+        <router-link to="/home/List" class="text">展示列表</router-link>
+        <span>|</span>
+        <router-link to="/home/choose" class="text">你的选择</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.text {
+    color: rgb(76, 193, 135);
+    width: 200px;
+    padding: 5px;
+}
+</style>
