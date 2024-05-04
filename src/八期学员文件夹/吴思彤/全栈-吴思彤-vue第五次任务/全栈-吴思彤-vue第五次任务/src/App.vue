0c9808921f35eafb186d800149024c4b92f7b983
Wu Si Tong <3080601439@qq.com>
2024-05-05 00:04:47 +0800

    全栈-吴思彤-vue第五次任务

---
+++ src/八期学员文件夹/吴思彤/全栈-吴思彤-vue第五次任务/全栈-吴思彤-vue第五次任务/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <h2>App Content</h2>
+    <div class="nav">
+      <router-link to="/home">home</router-link>
+      <router-link to="/about">about</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+  .app {
+    position: relative;
+    top: 130px;
+    left: 35%;
+  }
+</style>
