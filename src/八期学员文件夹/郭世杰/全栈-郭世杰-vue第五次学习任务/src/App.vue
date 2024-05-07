e86221454cf379e9df4b9763ba40ca44fb13b1ad
12guoshijie <154291163+12guoshijie@users.noreply.github.com>
2024-05-07 11:32:56 +0800

    全栈-郭世杰-vue第五次学习任务

---
+++ src/八期学员文件夹/郭世杰/全栈-郭世杰-vue第五次学习任务/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <router-link to="/home">Home组件</router-link>
+    <router-link to="/about">About组件</router-link>
+    <router-view></router-view>
+  </div>
+
+    
+</template>
+
+<style scoped>
+  
+</style>
---
+++ src/八期学员文件夹/郭世杰/全栈-郭世杰-vue第五次学习任务/src/components/About.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>我是about组件</div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+
+</style>
